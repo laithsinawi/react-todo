d8cdea6dec7374d6377fb0643ba40ce1ab620fc6
Laith Sinawi <laith@laithsinawi.com>
2018-06-26 16:37:45 -0700

    add TodoList

--- src/components/AddTodo.js
+++ src/components/AddTodo.js
@@ -1,14 +1,29 @@
 import React, { Component } from "react";
+import { connect } from 'react-redux';
+import { addTodo } from '../actions';
 
-export default class AddTodo extends Component {
+class AddTodo extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  handleOnSubmit(e){
+    e.preventDefault();
+    const task = this.refs.task.value;
+
+    this.props.dispatch(addTodo(task));
+  }
+
   render() {
     return (
       <div>
-        <form>
-          <input type="text" />
+        <form onSubmit={this.handleOnSubmit.bind(this)}>
+          <input type="text" ref="task" />
           <button>Add Todo</button>
         </form>
       </div>
     );
   }
 }
+
+export default connect()(AddTodo);
---
+++ src/components/TodoList.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react'
+
+class TodoList extends Component {
+    render() {
+        return (
+            <div>
+                Todo List
+                <ul>
+                    <li>item 1</li>
+                    <li>item 2</li>
+                    <li>item 3</li>
+                </ul>
+            </div>
+        )
+    }
+}
+
+export default TodoList;
